913a7c81b343b61b027b559ebadd0bafef7058b8
Julius_Basas <juliusb@cyzennt.co.jp>
2025-11-14 17:32:05 +0800

    inventory details can now be viewable in admin side

--- Borrowhen/src/main/resources/static/script/inventory/a-inventory-api.js
+++ Borrowhen/src/main/resources/static/script/inventory/a-inventory-api.js
@@ -132,7 +132,7 @@ async function loadInventories(page = 0, search = "") {
 			    <!-- View Button -->
 			    <div class="tooltip-wrapper relative group">
 			      <a
-			        href="#"
+			        href="/admin/inventory/details?encryptedId=${inventory.encryptedId}"
 			        class="view-btn border border-gray-300 hover:bg-gray-200 flex items-center justify-center h-8 w-8 rounded-md bg-blue-100 hover:bg-blue-200 transition shadow-sm"
 			        data-id="${inventory.encryptedId}"
 			        aria-label="View"
@@ -188,20 +188,6 @@ async function loadInventories(page = 0, search = "") {
 
 
         `;
-
-                // Attach event listeners
-                row.querySelector(".edit-btn").addEventListener("click", function() {
-                    const form = document.querySelector("#editForm");
-                    form.querySelector("#hiddenEncryptedId").value =
-                        this.getAttribute("data-id");
-                    form.submit();
-                });
-
-                row.querySelector(".view-btn").addEventListener("click", function() {
-                    const encryptedId = this.getAttribute("data-id");
-                    window.location.href =
-                        "/admin/inventory/details?encryptedId=" + encryptedId;
-                });
 
                 fragment.appendChild(row);
             });
